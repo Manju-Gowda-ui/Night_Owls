ad16e72be8482221c83d8c249f0f529b1b0e5040
HOME UEFI 21H2 <HOME UEFI 21H2@192.168.1.46>
2025-08-12 22:14:16 +0530

    Commiting Our Project to Local Repo - Night_OWls Repo

---
+++ 5pmbatches/src/Abstracttopics/Mainclass.java
@@ -0,0 +1,28 @@
+package Abstracttopics;
+
+abstract class Tester {
+	abstract void cool();
+
+	abstract void test();
+}
+
+abstract class Sample extends Tester {
+	void cool() {
+		System.out.println("Hey i am cool");
+	}
+}
+
+class Sample1 extends Sample {
+	void test() {
+		System.out.println("Hey i am test");
+	}
+}
+
+class Mainclass {
+	public static void main(String[] args) 
+	{
+		Sample1 s1 = new Sample1();
+		s1.cool();
+		s1.test();
+	}
+}
